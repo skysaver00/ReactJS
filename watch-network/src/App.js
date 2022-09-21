329bcb38405dc404722fff776372d3f19efdbc2a
skysaver00 <skysaver00@naver.com>
2022-09-22 01:52:27 +0900

    2022-09-22

--- watch-network/src/App.js
+++ watch-network/src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome Back!</h1>
+      <h1>Welcome Back!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
