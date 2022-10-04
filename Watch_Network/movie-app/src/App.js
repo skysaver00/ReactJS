4122c882c5c987e916cc1c9080cbf8543236cae5
skysaver00 <skysaver00@naver.com>
2022-10-05 00:28:20 +0900

    2022-10-05

--- Watch_Network/movie-app/src/App.js
+++ Watch_Network/movie-app/src/App.js
@@ -6,10 +6,9 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("i run all the time");
-  const iRunOlyOnce = () => {
-    console.log("i run only once.");
-  }
-  useEffect(iRunOlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API.......")
+  }, [])
   return (
     <div className="App">
       <h1 className={styles.title}>Welcome Back!!</h1>
