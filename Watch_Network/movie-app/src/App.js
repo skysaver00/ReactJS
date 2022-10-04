fa2dc46745fd03770475ef7248125746176fbf82
skysaver00 <skysaver00@naver.com>
2022-10-05 00:09:58 +0900

    2022-10-05

--- Watch_Network/movie-app/src/App.js
+++ Watch_Network/movie-app/src/App.js
@@ -1,10 +1,20 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOlyOnce = () => {
+    console.log("i run only once.");
+  }
+  useEffect(iRunOlyOnce, []);
   return (
     <div className="App">
       <h1 className={styles.title}>Welcome Back!!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
       <Button text={"Continue"} />
     </div>
   );
